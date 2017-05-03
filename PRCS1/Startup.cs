a5a242349a9baafc5a3ebdf999be42adf0d84ea4
Muhammad Asif <asif4347@icloud.com>
2017-05-03 14:09:44 -0700

    RDBC

---
+++ PRCS1/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using PRCS.Models;
+using PRCS1.Models;
+
+[assembly: OwinStartupAttribute(typeof(PRCS1.Startup))]
+namespace PRCS1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            
+        }
+      
+    }
+}
